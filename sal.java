a68628ab19cff15a4fada003f0f7ee08a81336b7
priya katyal <priyakatyal07@gmail.com>
2024-11-27 22:55:00 +0530

    java_prog

---
+++ sal.java
@@ -0,0 +1,14 @@
+class sal {
+    public static void main(String[] args) {
+        int a=13000;
+        if (a>=10000){
+            a=a+2000;
+            System.out.println(a);
+        }
+        else{
+            a=a+1000;
+            System.out.println(a);
+        }
+    }
+    
+}
