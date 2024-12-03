53587b4b4c48beb9fff37fda2201c7522c2b92df
priya katyal <priyakatyal07@gmail.com>
2024-12-03 22:40:18 +0530

    14th_prog

---
+++ onedarray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class onedarray {
+    public static void main(String[] args) {
+    Scanner in =new Scanner(System.in);
+    int[] arr=new int[5];
+    System.out.print("enter the 5 number :");
+
+    for(int i=0;i<arr.length;i++){
+        arr[i]=in.nextInt();
+    }
+    System.out.println(Arrays.toString(arr));
+    System.out.println(arr[3]);
+    // System.out.println("the alements in array is :");
+    // for(int i=0;i<arr.length;i++){
+    //     System.out.print(arr[i]+" ");
+
+
+    
+    
+}}
