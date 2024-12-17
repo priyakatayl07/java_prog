a6b65577a166b71d36080505cd1ade2309f3a5ea
priya katyal <priyakatyal07@gmail.com>
2024-12-17 16:44:12 +0530

    max_ele

--- maxelement.java
+++ maxelement.java
@@ -1,32 +1,20 @@
 import java.util.*;
-public class maxelement {
+class maxelement{
     public static void main(String[] args) {
-        Scanner in =new Scanner(System.in);
-        int[] arr=new int[5];
-        System.out.print("enter the elements:");
-        for(int i =0;i<arr.length ;i++){
+        Scanner in=new Scanner(System.in);
+        int []arr=new int[5];
+        System.out.println("enter the values of  arr:");
+        for(int i=0;i<arr.length;i++){
             arr[i]=in.nextInt();
         }
-            int max=arr[0];
-            for(int i=0;i<arr.length;i++){
-                if(arr[i]>max){
-                    max=arr[i];
-
-                }}
-            System.out.println("the max element in array is :"+max);
-            int min=arr[0];
-            for(int i=0;i<arr.length;i++){
-                if(arr[i]<min){
-                    min=arr[i];
-                }
+        System.out.println(Arrays.toString(arr));
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
             }
-            System.out.println("the min element in array is:"+min);
-
-
- }
+        }
+        System.out.println("max element is :"+max);
 
     }
-
-
-    
-
+}
