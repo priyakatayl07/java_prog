37159c46b779a8f30e21a0c73ff60e371866abe2
priya katyal <priyakatyal07@gmail.com>
2024-12-18 22:23:34 +0530

    18th_prog

---
+++ findele.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class findele {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int [] arr=new int[5];
+        System.out.print("enter the elements of array:");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=in.nextInt();
+            if(arr[i]==14){
+                System.out.println("yes 14 is present in the array");
+                break;
+
+            }
+        }        
+    }
+    
+}
